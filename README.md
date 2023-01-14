c3842df0933956513a332e54bddecbc3c71333ea
waaaaall <nm7wall@gmail.com>
2023-01-14 22:38:52 +0900

    complete

---
+++ README.md
@@ -0,0 +1,2 @@
+# copy-markdown-link
+refs: https://r7kamura.com/articles/2022-05-18-learn-chrome-extention-in-y-minutes
--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,24 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
+import "./style.css";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+function copy({ plain }: { plain: string }) {
+  const listener = (event: any) => {
+    event.clipboardData.setData("text/plain", plain);
+    event.preventDefault();
+  };
+  document.addEventListener("copy", listener);
+  document.execCommand("copy");
+  document.removeEventListener("copy", listener);
+}
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+chrome.tabs.query(
+  {
+    active: true,
+    currentWindow: true,
+  },
+  (tabs) => {
+    const { title, url } = tabs[0];
+    copy({
+      plain: `[${title}](${url})`,
+    });
+  }
+);
